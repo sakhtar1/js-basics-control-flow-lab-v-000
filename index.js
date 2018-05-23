6807de66bfd70a748709515e91cefd414436f2d8
Sharmina Akhtar <sakht318@gmail.com>
2018-05-23 06:06:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  } else if (1999 < someValue && someValue < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (someValue < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else (someValue > 2500){
+    return 'No can do.';
+  }
+
+}
+
 function scuberGreetingForFeet (feet) {
   if (feet <= 400) {
     return 'This one is on me!';
