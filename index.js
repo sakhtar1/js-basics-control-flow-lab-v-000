fd889a6862d7c4f9aa08564de5fc374295b528bf
Sharmina Akhtar <sakht318@gmail.com>
2018-05-23 06:07:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,16 +9,6 @@ function scuberGreetingForFeet(someValue) {
   }
 }
 
-function scuberGreetingForFeet (feet) {
-  if (feet <= 400) {
-    return 'This one is on me!';
-  } else if (1999 < feet && feet < 2500) {
-    return "I will gladly take your thirty bucks.";
-  } else {
-    return "No can do.";
-  }
-}
-
 function ternaryCheckCity(city) {
   var city = false;
   if (city === true) {
