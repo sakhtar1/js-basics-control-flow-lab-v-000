ba28baa43db11a6446869dda2cbca69b8552bc77
Sharmina Akhtar <sakht318@gmail.com>
2018-05-23 06:02:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
-  let message
+
   if (someValue <= 400) {
-    message = 'This one is on me!';
+    return 'This one is on me!';
   } else if (someValue > 2000) {
-    message = "I will gladly take your thirty bucks.";
+    return "I will gladly take your thirty bucks.";
   } else if (someValue < 2500) {
-    message = "I will gladly take your thirty bucks.";
-  }else (someValue > 2500){
-    message = 'No can do.';
+    return "I will gladly take your thirty bucks.";
+  } else (someValue > 2500){
+    return 'No can do.';
   }
   return message
 }
