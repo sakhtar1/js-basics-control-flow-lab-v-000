65c090b150a25b4a1bd5a10f96f4a21407617ff6
Sharmina Akhtar <sakht318@gmail.com>
2018-05-23 06:07:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ function scuberGreetingForFeet(someValue) {
     return 'This one is on me!';
   } else if (1999 < someValue && someValue < 2500) {
     return "I will gladly take your thirty bucks.";
-  } else if (someValue < 2500) {
-    return "I will gladly take your thirty bucks.";
   } else (someValue > 2500){
     return 'No can do.';
   }
