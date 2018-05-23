dd65789960da5ddc11809561f7611170cfe9cb21
Sharmina Akhtar <sakht318@gmail.com>
2018-05-23 05:38:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 function scuberGreetingForFeet(someValue) {
   let message
   if (someValue <= 400) {
-    message = 'This one is on me!'
+    message = 'This one is on me!';
   } else if (someValue > 2000) {
-    message = "I will gladly take your thirty bucks."
+    message = "I will gladly take your thirty bucks.";
   } else if (someValue < 2500) {
-    message = "I will gladly take your thirty bucks."
+    message = "I will gladly take your thirty bucks.";
   }else (someValue > 2500){
-    message = 'No can do.'
+    message = 'No can do.';
   }
   return message
 }
@@ -16,9 +16,9 @@ function scuberGreetingForFeet(someValue) {
 function ternaryCheckCity(city) {
   let message
   if (city === "NYC") {
-    message = "Ok, sounds good."
+    message = "Ok, sounds good.";
   } else {
-    message = "No go."
+    message = "No go.";
   }
   return message
 }
@@ -26,11 +26,11 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip) {
   let message
   if (tip === "generous") {
-    message = "Thank you so much."
+    message = "Thank you so much.";
   } else if (tip === 'not as generous') {
-    message = "Thank you."
+    message = "Thank you.";
   } else {
-    message = "Bye."
+    message = "Bye.";
   }
   return message
 }
